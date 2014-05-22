93b91f23b7dc92ef2964cf9b438a9509c55ab361
Sam Makagon <sam_makagon@trepp.com>
2014-05-22 10:00:11 -0400

    bugfixes

--- conureboard/views.py
+++ conureboard/views.py
@@ -17,10 +17,10 @@ def show_services():
     print "offset=%s, limit=%s" % (offset, ITEMS_PER_PAGE)
 
   #base services query
-  services = session.query(models.Service, models.Status).\
+  services = session.query(models.Service).\
               join(models.Status).\
-              with_entities(models.Service.name, models.Service.host, models.Service.status, models.Service.modify_date, models.Service.start_date,\
-                models.Service.stop_date, models.Service.message, models.Service.arguments, models.Status.description, models.Status.id )
+              with_entities(models.Service.id, models.Service.name, models.Service.host, models.Service.status, models.Service.modify_date, models.Service.start_date,\
+                models.Service.stop_date, models.Service.message, models.Service.arguments, models.Status.description )
   
   #apply search
   if request.args.get('search'):
@@ -32,7 +32,7 @@ def show_services():
     if request.args.get('sort_order') == 'desc':
       services = services.order_by(getattr(models.Service, request.args.get('sort_by')).desc())
     else:
-      services = services.order_by(getattr(models.Service, request.args.get('sort_by')))
+      services = services.order_by(models.Service.modify_date.desc())
   else:
     #default sort
     services = services.order_by(models.Service.modify_date.desc())
@@ -52,7 +52,7 @@ def show_services():
 
 
 
-@app.route('/events/<int:service_id>')
+@app.route('/services/<int:service_id>')
 def show_events(service_id):
   cur_page = get_page(request)
   offset = get_offset(cur_page)
