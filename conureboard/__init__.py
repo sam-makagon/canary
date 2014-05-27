cff3158a964187f1f79fa2f140ff786d58e44be7
Sam Makagon <sam_makagon@trepp.com>
2014-05-27 16:41:51 -0400

    config

--- conureboard/__init__.py
+++ conureboard/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
 
-from conureboard import database, models, views, api
+from conureboard import database, models, views, api, config
