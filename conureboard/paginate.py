f0957dd193a7b22822dd45e31b71333957ddfbc0
Sam Makagon <sam_makagon@trepp.com>
2014-05-28 14:37:16 -0400

    move vars to config

--- conureboard/paginate.py
+++ conureboard/paginate.py
@@ -1,10 +1,9 @@
+from __future__ import division
 import math
-from decimal import Decimal
-
-ITEMS_PER_PAGE = 50
+from conureboard.config import ITEMS_PER_PAGE
 
 def get_total_pages(record_count):
-  return int(math.ceil(Decimal(record_count / ITEMS_PER_PAGE )))
+  return int(math.ceil(record_count / ITEMS_PER_PAGE))
 
 def get_offset(cur_page):
   return (cur_page-1) * ITEMS_PER_PAGE
