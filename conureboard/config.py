ad97bc293447aec7b25d5fba26ba444a56d1acb6
Sam Makagon <sam_makagon@trepp.com>
2014-05-28 17:31:58 -0400

    added conureboard.cfg

--- conureboard/config.py
+++ conureboard/config.py
@@ -1,3 +1,22 @@
-DB_ENGINE = 'sqlite:///app.db'
-ITEMS_PER_PAGE = 50
-DEBUG = 1
+import os
+from ConfigParser import SafeConfigParser,NoOptionError
+parser = SafeConfigParser()
+parser.read('conureboard/conureboard.cfg')
+
+#SQLAlchemy database connection string
+try:
+  DB_ENGINE = parser.get('DEFAULT','DB_ENGINE')
+except NoOptionError:
+  DB_ENGINE = 'sqlite:///app.db'
+
+#how many items per page to display on service and events pages
+try:
+  ITEMS_PER_PAGE = parser.get('DEFAULT','ITEMS_PER_PAGE')
+except NoOptionError:
+  ITEMS_PER_PAGE = 50
+
+#log sql statements and arguments to STDERR; flask.log when running under WSGI
+try:
+  DEBUG = parser.get('DEFAULT','DEBUG')
+except NoOptionError:
+  DEBUG = 0
