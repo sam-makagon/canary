9abb03797b31e0dc8e176c3b7637cf5de6534295
Sam Makagon <sam_makagon@trepp.com>
2014-06-05 11:53:21 -0400

    set start/stop date correctly in service

--- canary/__init__.py
+++ canary/__init__.py
@@ -4,6 +4,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+EVENT_START = 0
+EVENT_STOP  = 1
+EVENT_INFO  = 2
+
 def logmsg(msg):
   print (msg, file=sys.stderr)
 
--- canary/api.py
+++ canary/api.py
@@ -1,6 +1,6 @@
 import flask.ext.restless
 
-from canary import app, models, logmsg
+from canary import app, models, logmsg, EVENT_START, EVENT_STOP
 from canary.database import session, printquery
 from canary.paginate import get_total_pages, get_offset
 from canary.config import ITEMS_PER_PAGE, DEBUG
@@ -11,11 +11,17 @@ from canary.database import session
 # update service based on new event
 def update_service(data=None, **kw):
   if DEBUG:
-    logmsg("data=%s, kw=%s" % (data, kw['result']['status'])) 
+    logmsg("data=%s, kw=%s" % (data, kw)) 
 
   update = {'status': kw['result']['status']}
   if kw['result']['message']:
     update['message'] = kw['result']['message']
+  
+  if kw['result']['event'] == EVENT_START:
+    update['start_date'] = kw['result']['modify_date']
+  elif kw['result']['event'] == EVENT_STOP:
+    update['stop_date'] = kw['result']['modify_date']
+
   try:
     session.query(models.Service).filter(models.Service.id == kw['result']['parent_id']).update(update)
     session.commit()
